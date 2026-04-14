22e247de213585ac054fa361d33a71df11965306
jaival-11 <251092232+jaival-11@users.noreply.github.com>
2026-04-14 08:23:56 +0530

    updated to web ui

--- bot.js
+++ bot.js
@@ -292,8 +292,7 @@ const HTML_UI = `
 
     <div style="font-size: 0.85em; color: #666; font-family: sans-serif; line-height: 1.4;">
         &copy; 2026 Jaival. <br>
-        Distributed under the MIT License. <br>
-        Free for personal and commercial use.
+        Distributed under the MIT License.
     </div>
 </footer>
 
